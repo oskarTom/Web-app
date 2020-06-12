f8db4e6b919ebd489ace4dcadb1fe2018aecc96f
Tom Rindell <tomoskar@protonmail.com>
2020-06-12 23:44:41 +0300

    Updated Post table

--- src/main/java/projekti/database/PostRepository.java
+++ src/main/java/projekti/database/PostRepository.java
@@ -3,5 +3,5 @@ package projekti.database;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    Account FindByPoster(Account poster);
 }
